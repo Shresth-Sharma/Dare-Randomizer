a87c26a0082b56ddb67f94c41c750cf5caf397d6
Shresth-Sharma <76861631+Shresth-Sharma@users.noreply.github.com>
2021-12-30 18:25:06 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -6,6 +6,7 @@ var w1,w2,w3,w4,w5,w6,w7,w8,w9,w10,w11,w12,w13,w14,w15,w16,w17,w18,w19,w20;
 var gameState=0,number=0;
 var ran;
 function setup(){
+    alert("File has been created by Shresth Sharma and "+"All the copyrights are reserved");
     createCanvas(800,600);
     sprites();
 }
